2d6d6beb630cb85f34fdd54496f24cac905e4ee8
collinsmithbrooks <scollin@live.com>
2019-05-15 20:50:51 -0500

    Section 4

--- DatingApp.Api/Controllers/ValuesController.cs
+++ DatingApp.Api/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace DatingApp.API.Controllers
 
         }
         // GET api/values
-        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -30,7 +30,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
-        [AllowAnonymous]
+        
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
