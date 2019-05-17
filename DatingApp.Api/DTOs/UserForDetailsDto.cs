02ea3fdf5a310f69d1351cc0287391a39ae52916
collinsmithbrooks <scollin@live.com>
2019-05-16 20:53:59 -0500

    End of Section 8

--- DatingApp.Api/DTOs/UserForDetailsDto.cs
+++ DatingApp.Api/DTOs/UserForDetailsDto.cs
@@ -1,21 +1,16 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.Api.Model;
 
-namespace DatingApp.Api.Model
+namespace DatingApp.Api.DTOs
 {
-    public class User
-    {
-        public int Id { get; set; }
+    public class UserForDetailsDto
+    {public int Id { get; set; }
 
         public string Username { get; set; }
-
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }
-
         public string Gender{get;set;}
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
@@ -30,10 +25,12 @@ namespace DatingApp.Api.Model
         public string City {get; set;}
 
         public string Country { get; set; }
-
-        public ICollection<Photo> Photos { get; set; }
-
         public DateTime LastActive { get; set; }
 
+        public string PhotoUrl { get; set; }
+
+        public ICollection<PhotoForDetailDto> Photos { get; set; }
+
+        
     }
 }
---
+++ DatingApp.Api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.Api.DTOs;
+using DatingApp.Api.Model;
+
+namespace DatingApp.Api.Helpers
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User,UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.ResolveUsing( d => d.DateOfBirth.CalculateAge()) );
+            CreateMap<User,UserForDetailsDto>() 
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.ResolveUsing( d => d.DateOfBirth.CalculateAge()) );
+            CreateMap<Photo,PhotoForDetailDto>();
+        }
+    }
+}
